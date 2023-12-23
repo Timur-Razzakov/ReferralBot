7b5b3d8ba3295c120bf9bed0322e4a2864459e37
baxti <baxtiyortairov1@gmail.com>
2023-12-23 21:50:46 +0500

    Finished

--- config/settings.py
+++ config/settings.py
@@ -94,21 +94,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # # Подключаем базу данных, в нашем случае это PostgreSQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASS'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('PORT')
-#     }
-# }
-# Подключаем базу данных, в нашем случае это PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('PORT')
     }
 }
 
