5197f51c5e04847363b313c8c37a563f2ccb9780
baxti <baxtiyortairov1@gmail.com>
2023-12-23 22:32:02 +0500

    Finished

--- bot_ref/models.py
+++ bot_ref/models.py
@@ -19,6 +19,7 @@ class User(models.Model):
         'User',
         on_delete=models.SET_NULL,
         null=True,
+        blank=True,
         related_name='referrals',
         verbose_name='Кто пригласил'
     )
